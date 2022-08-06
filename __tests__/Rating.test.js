02004e52a6cd2cfb481e77beb769eccbddc16a3e
Kevin <kelott@gmx.de>
2022-08-06 15:18:20 +0200

    add some comments from help request

--- __tests__/Rating.test.js
+++ __tests__/Rating.test.js
@@ -4,20 +4,25 @@ import '@testing-library/jest-dom';
 
 describe('Rating', () => {
   it('renders rating component', () => {
-    render(<Rating rating={3} />);
+    const { rerender } = render(<Rating rating={3} />);
     expect(screen.getAllByAltText('star svg').length).toBe(5);
-    cleanup();
-    render(<Rating />);
+    screen.debug();
+
+    rerender(<Rating />);
     expect(screen.getAllByAltText('star svg').length).toBe(5);
-    cleanup();
   });
 
   it('should work with numbers between 0 and 5', () => {
     // should I refactor to make the test fail?
+    // Alex's suggestion: make component transform too large numbers to 5 and too small numbers to 0
     const testRating = [-127, 0, 5, 3.8, 6, 10, 100065];
     testRating.forEach((number) => {
       render(<Rating rating={number} />);
       cleanup();
     });
   });
+
+  test.each();
 });
+// msw package => mocking
+// "stop mocking fetch" article
