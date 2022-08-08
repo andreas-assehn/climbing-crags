4d3a2717c23bf07aed6f6ba9896e4c44a9bf617a
Kevin <kelott@gmx.de>
2022-08-08 19:39:05 +0200

    convert pages/api/routes/index to ts

---
+++ pages/api/routes/index.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import clientPromise from '../../../utils/mongodb';
+
+//TODO Add secret key to prevent unauthorized access
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const body = req.body;
+  if (req.method === 'PUT') {
+    if (!body) {
+      return res.status(400).json({ error: 'Bad request (No body)' });
+    }
+    if (!body.id || !body.imageSrc || !body.path || !body.cloudinaryId) {
+      return res.status(400).json({ error: 'Bad request (Missing fields)' });
+    }
+    try {
+      const client = await clientPromise;
+      const db = client.db('Climbing-crags');
+      const routesCollection = db.collection('routes');
+      const routesCursor = routesCollection.findOneAndUpdate(
+        { id: body.id },
+        {
+          _$push: {
+            images: {
+              $each: [{ src: body.imageSrc, id: body.cloudinaryId }],
+              $position: 0,
+            },
+          },
+        }
+      );
+      return res.status(200).json(routesCursor);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+
+  return res.status(400).json({ error: 'Bad request (Wrong method)' });
+}
