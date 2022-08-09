83dbfefc46c9f69c9c5fc8972f024d98f289c296
Kevin <kelott@gmx.de>
2022-08-09 11:30:50 +0200

    finish InfoCard tests

--- __tests__/InfoCard.test.js
+++ __tests__/InfoCard.test.js
@@ -2,12 +2,19 @@ import { cleanup, render, screen } from '@testing-library/react';
 import InfoCard from '../components/ui/InfoCard';
 import '@testing-library/jest-dom';
 
+afterEach(cleanup);
+
 describe('InfoCard', () => {
   it('should render InfoCard', () => {
     render(<InfoCard />);
     expect(screen.getByText('Grades:')).toBeInTheDocument();
-    cleanup();
   });
 
-  it('should show the correct stars', () => {});
+  it('should show N° Sectors or Routes depending on type passed', () => {
+    const { rerender } = render(<InfoCard type={'crag'} />);
+    expect(screen.getByText('N° Sectors:')).toBeInTheDocument();
+
+    rerender(<InfoCard type={'sector'} />);
+    expect(screen.getByText('N° Routes:')).toBeInTheDocument();
+  });
 });
