ba633d7e5b760a283fbb1236c98bd07fcb75ad03
Kevin <kelott@gmx.de>
2022-08-09 21:09:59 +0200

    finish tests for route and sector url + id

--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -1,5 +1,3 @@
-const { get } = require('http');
-
 describe('Basic functionality', () => {
   it('Loads index page', () => {
     cy.visit('/');
@@ -17,9 +15,70 @@ describe('Basic functionality', () => {
 
   it('shows a route page when entering a valid /route/:routeNumber url', () => {
     cy.visit('/route/4-6-2148');
-    cy.get('h1').should('contain', 'Carina');
-    cy.contains('Carina');
+    cy.contains('carina')
+      .next()
+      .find('a')
+      .should('contain', 'Cajenna')
+      .should('contain', 'MUZZERONE');
+    cy.contains('Grade:').should('contain', '5c');
+    cy.contains('Rating:').parent().as('stars');
+    cy.get('@stars').within(() => {
+      cy.get('img').should('have.length', '10');
+    });
+    cy.get('@stars')
+      .parent()
+      .within(() => {
+        cy.get('button').filter(':visible');
+      });
+    cy.contains('Comments')
+      .parent()
+      .as('commentsSection')
+      .should('contain', 'Average comment rating:')
+      .should('contain', 'How was it ?');
+    cy.get('@commentsSection').within(() => {
+      cy.get('input').invoke('attr', 'placeholder').should('eq', 'Title...');
+      cy.get('textarea').invoke('attr', 'placeholder').should('eq', 'Comment');
+      cy.get('img').should('have.length', '10');
+      cy.get('button').should('contain', 'SEND');
+    });
   });
-  it('shows a sector page when entering a valid /sector/:sectorNumber url', () => {});
+
+  it('shows a sector page when entering a valid /sector/:sectorNumber url', () => {
+    cy.visit('/crag/erto');
+    cy.contains('Erto').next().should('contain', 'ITA');
+    cy.contains('N° Sectors:').should('contain', '3');
+    cy.contains('Grades:').should('contain', '3a - 7a+');
+    cy.contains("People's rating:").parent().as('card');
+    cy.get('@card').within(() => {
+      cy.get('img').should('have.length', '10');
+    });
+    cy.contains('Sectors');
+    cy.contains('BIG').parent().as('bigParent');
+    cy.get('@bigParent').within(() => {
+      cy.get('img').should('have.length', '2');
+      cy.get('a');
+      cy.contains('6a - 6c');
+      cy.contains('10+ routes');
+      cy.contains('1.8');
+    });
+    cy.contains('No big').parent().as('noBigParent');
+    cy.get('@noBigParent').within(() => {
+      cy.get('img').should('have.length', '2');
+      cy.get('a');
+      cy.contains('3a - 6a');
+      cy.contains('10+ routes');
+      cy.contains('1.6');
+    });
+    cy.contains('Contessa').parent().as('contessaParent');
+    cy.get('@contessaParent').within(() => {
+      cy.get('img').should('have.length', '2');
+      cy.get('a');
+      cy.contains('Erto,');
+      cy.contains('ITA');
+      cy.contains('5c - 7a+');
+      cy.contains('7+ routes');
+      cy.contains('1.0');
+    });
+  });
   it('shows a crag page when entering a valid /crag/:cragNumber url', () => {});
 });
