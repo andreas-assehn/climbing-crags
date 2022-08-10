bead85afec3befdecaa9b5e898d503c2bad05d79
Andreas Asséhn <andreas.assehn@gmail.com>
2022-08-10 20:12:42 +0200

    E2E done

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -13,11 +13,19 @@ describe('User flow test', () => {
   });
   it('Clicks "See more" on first element and checks url', () => {
     cy.contains('see more').click();
-    cy.url().should('include', '/sector/3342');
+    cy.url().should('include', '/sector/1834');
   });
   it('Clicks first element and checks url', () => {
     cy.get('a[href^="/route/"]').first().click();
-    cy.url().should('include', '/route/2-0-62');
+    cy.url().should('include', '/route/0-3-62');
+  });
+  it('Should navigate back and check url', () => {
+    cy.go('back');
+    cy.url().should('include', '/sector/1834');
+  });
+  it('Clicks the Climbing Crags link in navbar and checks url', () => {
+    cy.get('a[href^="/"]').first().click();
+    cy.url().should('eq', 'http://localhost:3000/');
   });
 });
 
