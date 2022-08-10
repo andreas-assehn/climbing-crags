c8ddbf09aa7c6af11aebd746327b29ddf197c2f2
Kevin <kelott@gmx.de>
2022-08-10 20:38:19 +0200

    extract cypress secrets to env file

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require('cypress');
+const { GoogleSocialLogin } = require('cypress-social-logins').plugins;
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('task', {
+        GoogleSocialLogin: GoogleSocialLogin,
+      });
+    },
+    baseUrl: 'http://localhost:3000',
+    chromeWebSecurity: false,
+  },
+
+  watchForFileChanges: true,
+
+  component: {
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack',
+    },
+  },
+});
